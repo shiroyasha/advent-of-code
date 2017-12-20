0d3524389e24accfb3be999b8e0e1ee4a44a7a8b
Igor Šarčević <igor@renderedtext.com>
2017-12-20 21:37:31 +0000

    Scanner part 1 and 2 with good performance

--- 013-scanner/src/main.rs
+++ 013-scanner/src/main.rs
@@ -1,143 +1,103 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
 #[derive(Debug)]
 struct Layer {
-    length : i32,
     position: i32,
-    forward: bool,
+    depth: i32,
 }
 
 impl Layer {
-    fn new(length : i32) -> Self {
-        Layer { length: length, position : 0, forward : true }
+
+    fn is_deadly(&self, delay : i32) -> bool {
+        let time = self.position + delay;
+        let pos = time % ((self.depth - 1) * 2);
+
+        // println!("{}, {}, {}", self.position, self.depth, pos);
+        // println!("{}, {}", time, pos);
+
+        pos == 0
     }
 
-    fn reset(&mut self) {
-        self.position = 0;
-        self.forward = true;
+    fn penatly(&self) -> i32 {
+        // println!("penalty: {}", self.position * self.depth);
+
+        self.position * self.depth
     }
 
-    fn advance(&mut self) {
-        if self.forward {
-            self.position += 1;
-        } else {
-            self.position -= 1;
-        }
-
-        if self.position == 0 {
-            self.forward = true;
-        }
-
-        if self.position == self.length - 1 {
-            self.forward = false;
-        }
-    }
 }
 
-fn display(layers : &HashMap<i32, Layer>, current_pos : i32) {
-    let layers_len = *layers.keys().max().unwrap() + 1;
+struct Layers {
+    layers : Vec<Layer>
+}
 
-    println!("---------------------");
+impl Layers {
+    fn new(input: &str) -> Self {
+        let mut layers = Vec::new();
 
-    for i in 0..layers_len {
-        if current_pos == i {
-            print!("({}): ", i);
-        } else {
-            print!(" {} : ", i);
-        }
+        input.lines().for_each(|l| {
+            let mut parts = l.split(":").map(|part| part.trim().parse().unwrap());
 
-        match layers.get(&i) {
-            Some(l) => {
-                for j in 0..l.length {
-                    if l.position == j {
-                        print!("[S] ");
-                    } else {
-                        print!("[ ] ");
-                    }
-                }
+            let position = parts.next().unwrap();
+            let depth    = parts.next().unwrap();
 
-                println!("");
-            },
-            None => {
-                println!("...");
+            layers.push(Layer { position : position, depth : depth });
+        });
+
+        Layers { layers : layers }
+    }
+
+    fn score(&self) -> i32 {
+       self.layers
+           .iter()
+           .filter(|l| l.is_deadly(0))
+           .map(|l| l.penatly())
+           .sum()
+    }
+
+    fn safe_delay(&self) -> i32 {
+        let mut delay = 0;
+
+        loop {
+            let has_dedly = self.layers.iter().any(|l| l.is_deadly(delay));
+
+            println!("{}", delay);
+
+            if !has_dedly {
+                break;
             }
-        }
-    }
-}
 
-fn calculate(start : i32, layers : &mut HashMap<i32, Layer>) -> (i32, bool) {
-    let mut score = 0;
-    let mut caught = false;
-    let mut current_pos : i32 = start;
-    let layers_len = *layers.keys().max().unwrap();
-
-    println!(" --> {}", start);
-
-    while current_pos <= layers_len {
-        match layers.get(&current_pos) {
-            Some(l) => {
-                if l.position == 0 {
-                    score += current_pos * l.length;
-                    caught = true;
-                }
-            },
-            None => {
-                ()
-            }
+            delay += 1;
         }
 
-        current_pos += 1;
-
-        layers.iter_mut().for_each(|(i, l)| l.advance());
+        delay
     }
-
-    (score, caught)
-}
-
-fn wait_time_to_be_safe(layers : &mut HashMap<i32, Layer>) -> i32 {
-    let mut wait = 0;
-
-    loop {
-        layers.iter_mut().for_each(|(i, l)| l.reset());
-
-        let caught = calculate(-wait, layers).1;
-
-        if !caught {
-            break;
-        }
-
-        wait += 1;
-    }
-
-    wait
 }
 
 #[test]
 fn calculate_test() {
-   let mut layers : HashMap<i32, Layer> = HashMap::new();
+    let input = "0: 3
+1: 2
+4: 4
+6: 4
+";
 
-   layers.insert(0, Layer::new(3));
-   layers.insert(1, Layer::new(2));
-   layers.insert(4, Layer::new(4));
-   layers.insert(6, Layer::new(4));
+    let layers = Layers::new(input);
 
-   println!("{:?}", layers);
-
-   assert_eq!(calculate(0, &mut layers).0, 24);
+    assert_eq!(layers.score(), 24);
 }
 
 #[test]
-fn wait_time_to_be_safe_test() {
-   let mut layers : HashMap<i32, Layer> = HashMap::new();
+fn safe_delay() {
+    let input = "0: 3
+1: 2
+4: 4
+6: 4
+";
 
-   layers.insert(0, Layer::new(3));
-   layers.insert(1, Layer::new(2));
-   layers.insert(4, Layer::new(4));
-   layers.insert(6, Layer::new(4));
+    let layers = Layers::new(input);
 
-   assert_eq!(wait_time_to_be_safe(&mut layers), 10);
+    assert_eq!(layers.safe_delay(), 10);
 }
 
 fn main() {
@@ -146,18 +106,6 @@ fn main() {
 
     file.read_to_string(&mut content).expect("can't read from file");
 
-    let mut layers : HashMap<i32, Layer> = HashMap::new();
-
-    content.lines().for_each(|l| {
-        let mut parts = l.split(":").map(|part| part.trim().parse().unwrap());
-
-        let index = parts.next().unwrap();
-        let length = parts.next().unwrap();
-
-        layers.insert(index, Layer::new(length));
-    });
-
-
-   println!("{}", calculate(0, &mut layers).0);
-   // println!("{}",wait_time_to_be_safe(&mut layers));
+   println!("{}", Layers::new(&content).score());
+   println!("{}", Layers::new(&content).safe_delay());
 }
