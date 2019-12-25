864b3de3976cdec8057e067604343dc40f300bdd
Igor Šarčević <igor@renderedtext.com>
2019-12-25 11:44:57 +0000

    Day 18 part 1

--- 2019/019-Tractor-Beam/main.go
+++ 2019/019-Tractor-Beam/main.go
@@ -26,6 +26,7 @@ const (
 )
 
 type Process struct {
+	code          []int
 	memory        []int
 	position      int
 	output        []int
@@ -43,6 +44,7 @@ func NewProcess(code []int, input []int) *Process {
 	copy(memory, code)
 
 	return &Process{
+		code:         code,
 		memory:       memory,
 		position:     0,
 		input:        input,
@@ -186,6 +188,144 @@ func (p *Process) Debug(name string, length int) {
 	} else {
 		fmt.Printf(" %15s- ", "")
 	}
+}
+
+func (p *Process) Opcodes() []string {
+	result := []string{}
+
+	i := 0
+
+	for {
+		instruction := p.code[i] % 100
+
+		fmt.Println(i, instruction)
+
+		modes := []int{-1, -1, -1}
+		modes[0] = (p.code[i] / 100) % 10
+		modes[1] = (p.code[i] / 1000) % 10
+		modes[2] = (p.code[i] / 10000) % 10
+
+		// toStr := func(name string, l int) string {
+		// 	res := fmt.Sprintf("%4d: %4s", i, name)
+
+		// 	for k := 1; k < l; k++ {
+		// 		if modes[k-1] == 0 {
+		// 			res += fmt.Sprintf(" [%4d] ", p.code[i+k])
+		// 			continue
+		// 		}
+
+		// 		if modes[k-1] == 1 {
+		// 			res += fmt.Sprintf("  %4d  ", p.code[i+k])
+		// 			continue
+		// 		}
+
+		// 		if modes[k-1] == 2 {
+		// 			res += fmt.Sprintf("  %4d^ ", p.code[i+k])
+		// 			continue
+		// 		}
+
+		// 		panic(modes[k-1])
+		// 	}
+
+		// 	return res
+		// }
+
+		param := func(i int, param int) string {
+			modes := []int{-1, -1, -1, -1}
+			modes[1] = (p.code[i] / 100) % 10
+			modes[2] = (p.code[i] / 1000) % 10
+			modes[3] = (p.code[i] / 10000) % 10
+
+			m := modes[param]
+			v := p.code[i+param]
+
+			if m == 0 {
+				return fmt.Sprintf("reg[%d]", v)
+			}
+
+			if m == 1 {
+				return fmt.Sprintf("%d", v)
+			}
+
+			if m == 2 {
+				if v == 0 {
+					return fmt.Sprintf("req[pointer]")
+				}
+
+				if v > 0 {
+					return fmt.Sprintf("req[pointer+%d]", v)
+				}
+
+				if v < 0 {
+					return fmt.Sprintf("req[pointer%d]", v)
+				}
+			}
+
+			panic("AAA")
+		}
+
+		switch instruction {
+		case OpcodeAdd:
+			str := fmt.Sprintf("%s = %s + %s", param(i, 3), param(i, 1), param(i, 2))
+
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+			i += 4
+		case OpcodeMultiply:
+			str := fmt.Sprintf("%s = %s * %s", param(i, 3), param(i, 1), param(i, 2))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 4
+		case OpcodeGetInput:
+			str := fmt.Sprintf("%s = getInput()", param(i, 1))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 2
+		case OpcodeWriteOutput:
+			str := fmt.Sprintf("puts(%s)", param(i, 1))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 2
+		case OpcodeJumpIfTrue:
+			str := fmt.Sprintf("if(%s == 1) goto %s", param(i, 1), param(i, 2))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 3
+		case OpcodeJumpIfFalse:
+			str := fmt.Sprintf("if(%s == 0) goto %s", param(i, 1), param(i, 2))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 3
+		case OpcodeLessThan:
+			str := fmt.Sprintf("if(%s < %s) { %s = 1 } else { %s = 0 }", param(i, 1), param(i, 2), param(i, 3), param(i, 3))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 4
+		case OpcodeEquals:
+			str := fmt.Sprintf("if(%s == %s) { %s = 1 } else { %s = 0 }", param(i, 1), param(i, 2), param(i, 3), param(i, 3))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 4
+		case OpcodeAdjustRelativeBase:
+			str := fmt.Sprintf("pointer = %s", param(i, 1))
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 2
+		case OpcodeHalt:
+			str := fmt.Sprintf("HALT")
+			result = append(result, fmt.Sprintf("%4d: %s", i, str))
+
+			i += 1
+		default:
+			i += 1
+			// goto a
+		}
+
+		if i >= len(p.code) {
+			break
+		}
+	}
+
+	return result
 }
 
 //
@@ -544,6 +684,15 @@ func check(pos Pos) int {
 	return p.output[len(p.output)-1]
 }
 
+func opcodes() {
+	code := []int{109, 424, 203, 1, 21101, 0, 11, 0, 1106, 0, 282, 21102, 18, 1, 0, 1106, 0, 259, 2101, 0, 1, 221, 203, 1, 21102, 1, 31, 0, 1105, 1, 282, 21101, 0, 38, 0, 1106, 0, 259, 20102, 1, 23, 2, 22101, 0, 1, 3, 21102, 1, 1, 1, 21101, 57, 0, 0, 1105, 1, 303, 2102, 1, 1, 222, 20101, 0, 221, 3, 21002, 221, 1, 2, 21101, 0, 259, 1, 21102, 1, 80, 0, 1105, 1, 225, 21102, 125, 1, 2, 21102, 1, 91, 0, 1106, 0, 303, 2101, 0, 1, 223, 21002, 222, 1, 4, 21102, 1, 259, 3, 21102, 225, 1, 2, 21102, 225, 1, 1, 21101, 0, 118, 0, 1106, 0, 225, 20102, 1, 222, 3, 21101, 0, 69, 2, 21102, 1, 133, 0, 1106, 0, 303, 21202, 1, -1, 1, 22001, 223, 1, 1, 21102, 148, 1, 0, 1106, 0, 259, 1201, 1, 0, 223, 20101, 0, 221, 4, 21001, 222, 0, 3, 21102, 1, 22, 2, 1001, 132, -2, 224, 1002, 224, 2, 224, 1001, 224, 3, 224, 1002, 132, -1, 132, 1, 224, 132, 224, 21001, 224, 1, 1, 21102, 195, 1, 0, 106, 0, 108, 20207, 1, 223, 2, 20101, 0, 23, 1, 21102, -1, 1, 3, 21101, 0, 214, 0, 1105, 1, 303, 22101, 1, 1, 1, 204, 1, 99, 0, 0, 0, 0, 109, 5, 1202, -4, 1, 249, 21202, -3, 1, 1, 22102, 1, -2, 2, 21201, -1, 0, 3, 21101, 250, 0, 0, 1106, 0, 225, 22102, 1, 1, -4, 109, -5, 2105, 1, 0, 109, 3, 22107, 0, -2, -1, 21202, -1, 2, -1, 21201, -1, -1, -1, 22202, -1, -2, -2, 109, -3, 2106, 0, 0, 109, 3, 21207, -2, 0, -1, 1206, -1, 294, 104, 0, 99, 22101, 0, -2, -2, 109, -3, 2106, 0, 0, 109, 5, 22207, -3, -4, -1, 1206, -1, 346, 22201, -4, -3, -4, 21202, -3, -1, -1, 22201, -4, -1, 2, 21202, 2, -1, -1, 22201, -4, -1, 1, 22102, 1, -2, 3, 21101, 0, 343, 0, 1106, 0, 303, 1105, 1, 415, 22207, -2, -3, -1, 1206, -1, 387, 22201, -3, -2, -3, 21202, -2, -1, -1, 22201, -3, -1, 3, 21202, 3, -1, -1, 22201, -3, -1, 2, 22102, 1, -4, 1, 21101, 384, 0, 0, 1106, 0, 303, 1106, 0, 415, 21202, -4, -1, -4, 22201, -4, -3, -4, 22202, -3, -2, -2, 22202, -2, -4, -4, 22202, -3, -2, -3, 21202, -4, -1, -2, 22201, -3, -2, 1, 21202, 1, 1, -4, 109, -5, 2105, 1, 0}
+	p := NewProcess(code, []int{})
+
+	for _, op := range p.Opcodes() {
+		fmt.Println(op)
+	}
+}
+
 func main() {
 	res := 0
 
@@ -562,4 +711,6 @@ func main() {
 	draw()
 
 	fmt.Println(res)
+
+	opcodes()
 }
